4b14e507b08c649215bb3d657e8b6fa30c0be0ce
willzhang <524869004@qq.com>
2019-07-11 22:46:07 +0800

    jms

--- oracle-weblogic/1036-jms/properties/config.sh
+++ oracle-weblogic/1036-jms/properties/config.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
-#export weblogic variable
 export ADMIN_NAME=AdminServer
 export DOMAIN_HOME=/root/Oracle/Middleware/user_projects/domains/base_domain/
-export ADMIN_URL=t3://0.0.0.0:7001
 export ADMIN_USERNAME=weblogic
 export ADMIN_PASSWORD=88888888
 export LISTEN_ADDRESS=0.0.0.0
-export LISTEN_PORT=7001
+export LISTEN_PORT=7002
 
 #config java memory
 export USER_MEM_ARGS="-Xms1g -Xmx2g -XX:PermSize=256m -XX:MaxPermSize=512m"
-
-#turnoff sso
-#export SSO_PATH=/root/Oracle/Middleware/user_projects/domains/base_domain/autodeploy/webapps/WEB-INF
-#cp -f /config/weblogic/app-config-acegi-security-dev.xml $SSO_PATH/app-config-acegi-security.xml
-#cp -f /config/weblogic/6002-core/env.properties $SSO_PATH/classes/
