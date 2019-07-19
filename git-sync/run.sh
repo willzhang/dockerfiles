9b7c7aa807d19546a1a7672eb80e0aab605ad9ee
Your Name <you@example.com>
2019-07-19 15:53:28 +0800

    update

--- git-sync/run.sh
+++ git-sync/run.sh
@@ -3,5 +3,4 @@ docker rm git-sync
 docker run -itd \
     --name git-sync \
     -e GIT_SYNC_REPO=git@github.com:willzhang/shell.git \
-    -e GIT_SYNC_SSH=true \
-    git-sync:v3.1.1
+    willdockerhub/git-sync:ssh
