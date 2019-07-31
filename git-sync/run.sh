35d0aea3f5f5f14bbb5c5393e04f433a7885abd9
willgithub <524869004@qq.com>
2019-07-31 20:25:25 +0800

    Update run.sh

--- git-sync/run.sh
+++ git-sync/run.sh
@@ -3,4 +3,5 @@ docker rm git-sync
 docker run -itd \
     --name git-sync \
     -e GIT_SYNC_REPO=git@github.com:willzhang/shell.git \
-    willdockerhub/git-sync:ssh
+    -e GIT_SYNC_SSH=true \
+    willdockerhub/git-sync:v3.1.1
