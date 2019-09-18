96aa3f145449e1e891e3e2dbc9bcf52897f7126a
willbkt <524869004@qq.com>
2019-09-19 01:18:29 +0800

    update

--- vagrant-box/build.sh
+++ vagrant-box/build.sh
@@ -1,2 +1,2 @@
-docker build -t willdockerhub/vagrant-box:centos .
-#docker build -t willdockerhub/vagrant-box:ubuntu .
+docker build -t willdockerhub/vagrant-box:centos1810 .
+#docker build -t willdockerhub/vagrant-box:ubuntu1904 .
